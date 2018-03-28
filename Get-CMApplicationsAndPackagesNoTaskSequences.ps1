731c1a054cea80c1a643e5df314e0c018f54c300
Matthew Bobke <mcbobke@gmail.com>
2018-03-28 01:13:26 -0700

    Formatting and removed command aliases

--- Get-CMApplicationsAndPackagesNoTaskSequences.ps1
+++ Get-CMApplicationsAndPackagesNoTaskSequences.ps1
@@ -1,24 +1,23 @@
 # Import the necessary module and CD to the CMS PSDrive
 Import-Module ConfigurationManager
-cd CMS:
+Set-Location CMS:
 
 Write-Output "Imported ConfigurationManager module and CD'ed to CMS."
 
 # Grab all non-deployed/0 dependent TS/0 dependent DT applications, and all packages (can't filter packages like applications)
-$AllRelevantApplications = Get-CMApplication | where {($_.IsDeployed -eq $False) `
-                                                        -and ($_.NumberofDependentTS -eq 0) `
-                                                        -and ($_.NumberofDependentDTs -eq 0)}
+$FinalApplications = Get-CMApplication `
+    | Where-Object {($_.IsDeployed -eq $False) -and ($_.NumberofDependentTS -eq 0) -and ($_.NumberofDependentDTs -eq 0)}
 $AllPackages = Get-CMPackage
 
 Write-Output "Grabbed relevant applications and all packages."
 
 # Grab all task sequences, filter to just a list of their references
-$TSReferences = Get-CMTaskSequence | select -ExpandProperty References
+$TSReferences = Get-CMTaskSequence | Select-Object -ExpandProperty References
 
 Write-Output "Grabbed all task sequences and filtered for their references."
 
 # Grab all deployments, filter to just a list of their package IDs
-$DeploymentPackageIDs = Get-CMDeployment | select -ExpandProperty PackageID
+$DeploymentPackageIDs = Get-CMDeployment | Select-Object -ExpandProperty PackageID
 
 Write-Output "Grabbed all deployments and filtered for their PackageIDs."
 
@@ -29,17 +28,6 @@ $FinalPackages = @()
 $FinalPackages = {$FinalPackages}.Invoke()
 
 Write-Output "Converted to .NET collection."
-
-<# NO LONGER NECESSARY DUE TO FILTERING DONE ABOVE
-# Filter applications to only those that do not have their PackageID or CI_UniqueID in the list of references
-foreach ($app in $AllRelevantApplications)
-{
-    if (($app.PackageID -notin $TSReferences) -or ($app.CI_UniqueID -notin $TSReferences))
-    {
-        $FinalApps.Add($app)
-    } 
-}
-#>
 
 # Filter packages to only those that do not have their PackageID in the list of references
 foreach ($package in $AllPackages)
@@ -53,7 +41,14 @@ foreach ($package in $AllPackages)
 Write-Output "Filtered packages through references and deployment PackageIDs."
 
 # Final output to CSV
-$AllRelevantApplications | select LocalizedDisplayName,PackageID,DateCreated,DateLastModified,IsDeployable,IsEnabled,IsExpired,IsHidden,IsSuperseded | sort -Property LocalizedDisplayName | Export-Csv -Path C:\temp\SCCM_Apps.csv
-$FinalPackages | select Name,PackageID,SourceDate,LastRefreshTime | sort -Property Name | Export-Csv -Path C:\temp\SCCM_Packages.csv
+$FinalApplications `
+    | Select-Object -Property LocalizedDisplayName,PackageID,DateCreated,DateLastModified,IsDeployable,IsEnabled,IsExpired,IsHidden,IsSuperseded `
+    | Sort-Object -Property LocalizedDisplayName `
+    | Export-Csv -Path "C:\temp\SCCM_Apps.csv"
 
-Write-Output "Output done."
+$FinalPackages `
+    | Select-Object Name,PackageID,SourceDate,LastRefreshTime `
+    | Sort-Object -Property Name `
+    | Export-Csv -Path "C:\temp\SCCM_Packages.csv"
+
+Write-Output "Output done and stored in C:\temp."
--- Get-SCCMInactiveDevices.ps1
+++ Get-SCCMInactiveDevices.ps1
@@ -1,19 +1,23 @@
+param(
+    [String]$AllDevicesCollectionID
+)
+
 # Import necessary modules
 Import-Module ActiveDirectory
 Import-Module ConfigurationManager
 
 # Get all Active Directory computer names
 Write-Host "Getting all Active Directory computer names..."
-cd AD:
+Set-Location AD:
 $AllADComputerObjects = Get-ADComputer -Filter * -ResultSetSize 15000 -Properties Name
-$AllADComputers = $AllADComputerObjects | select -ExpandProperty Name
+$AllADComputers = $AllADComputerObjects | Select-Object -ExpandProperty Name
 # Set -SearchBase to search in specific OUs
 # Set -Filter to search for specific property values
 
 # Get all SCCM device names
 Write-Host "Getting all SCCM computer names..."
-cd CMS:
-$AllCMComputers = Get-CMDevice -CollectionId "CMS00023" | select -ExpandProperty Name
+Set-Location CMS:
+$AllCMComputers = Get-CMDevice -CollectionId $AllDevicesCollectionID | Select-Object -ExpandProperty Name
 
 # Create .NET collection in preparation for filtering
 $AllMissingComputers = @()
