c71096d90b2f33c2768e996c4edfb61b85aa58d9
Matthew Bobke <mcbobke@gmail.com>
2018-05-06 21:10:22 -0700

    Update before blog post
    - Check for installed ConfigMgr module
    - Updated output coloring
    - Switch to using ArrayList instead of .NET Collection<PSObject>
    - CSV output path set to $PSScriptRoot

--- Get-CMApplicationsAndPackagesNoTaskSequences.ps1
+++ Get-CMApplicationsAndPackagesNoTaskSequences.ps1
@@ -1,33 +1,29 @@
-# Import the necessary module and CD to the CMS PSDrive
-Import-Module ConfigurationManager
-Set-Location CMS:
-
-Write-Output "Imported ConfigurationManager module and CD'ed to CMS."
+try {
+    Import-Module ConfigurationManager -ErrorAction 'Stop'
+    Set-Location CMS:
+}
+catch [System.IO.FileNotFoundException] {
+    throw 'The ConfigurationManager module cannot be found.'
+}
 
 # Grab all non-deployed/0 dependent TS/0 dependent DT applications, and all packages (can't filter packages like applications)
 $FinalApplications = Get-CMApplication `
     | Where-Object {($_.IsDeployed -eq $False) -and ($_.NumberofDependentTS -eq 0) -and ($_.NumberofDependentDTs -eq 0)}
 $AllPackages = Get-CMPackage
 
-Write-Output "Grabbed relevant applications and all packages."
+Write-Host "Grabbed relevant applications and all packages." -ForegroundColor Cyan
 
 # Grab all task sequences, filter to just a list of their references
 $TSReferences = Get-CMTaskSequence | Select-Object -ExpandProperty References
 
-Write-Output "Grabbed all task sequences and filtered for their references."
+Write-Host "Grabbed all task sequences and filtered for their references." -ForegroundColor Cyan
 
 # Grab all deployments, filter to just a list of their package IDs
 $DeploymentPackageIDs = Get-CMDeployment | Select-Object -ExpandProperty PackageID
 
-Write-Output "Grabbed all deployments and filtered for their PackageIDs."
+Write-Host "Grabbed all deployments and filtered for their PackageIDs." -ForegroundColor Cyan
 
-# Create PS array for final collection
-$FinalPackages = @()
-
-# The next line turns this PS array into a .NET collection
-$FinalPackages = {$FinalPackages}.Invoke()
-
-Write-Output "Converted to .NET collection."
+$FinalPackages = New-Object -TypeName 'System.Collections.ArrayList'
 
 # Filter packages to only those that do not have their PackageID in the list of references
 foreach ($package in $AllPackages) {
@@ -36,17 +32,16 @@ foreach ($package in $AllPackages) {
     } 
 }
 
-Write-Output "Filtered packages through references and deployment PackageIDs."
+Write-Host "Filtered packages through references and deployment PackageIDs." -ForegroundColor Cyan
 
-# Final output to CSV
 $FinalApplications `
     | Select-Object -Property LocalizedDisplayName, PackageID, DateCreated, DateLastModified, IsDeployable, IsEnabled, IsExpired, IsHidden, IsSuperseded `
     | Sort-Object -Property LocalizedDisplayName `
-    | Export-Csv -Path "C:\temp\SCCM_Apps.csv"
+    | Export-Csv -Path "$PSScriptRoot\SCCM_Apps.csv"
 
 $FinalPackages `
     | Select-Object Name, PackageID, SourceDate, LastRefreshTime `
     | Sort-Object -Property Name `
-    | Export-Csv -Path "C:\temp\SCCM_Packages.csv"
+    | Export-Csv -Path "$PSScriptRoot\SCCM_Packages.csv"
 
-Write-Output "Output done and stored in C:\temp."
+Write-Host "Done! CSVs stored in $PSScriptRoot." -ForegroundColor Green
